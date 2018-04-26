b0ae5a8a4899d04b9b520527c80f99dd23f6988e
Zhang haotian <zhtroy@sina.com>
2018-04-26 18:13:11 +0800

    init

---
+++ Assets/LevelEditor/Scripts/LevelData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace GeneralLevelEditor
+{
+    public class LevelData 
+    { 
+        #region 
+            
+        #endregion
+
+        public event Action<bool> selectedChanged;
+
+        private bool _selected;
+        public bool Selected
+        {
+            get { return _selected; }
+            set
+            {
+                // if the value has changed
+                if (_selected != value)
+                {
+                    // update the state and call the selection handler if it exists
+                    _selected = value;
+                    if (selectedChanged != null) selectedChanged(_selected);
+                }
+            }
+        }
+
+        public string levelName;
+    }
+}
---
+++ Assets/LevelEditor/Scripts/LevelListScrollerController.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using EnhancedUI.EnhancedScroller;
+
+namespace GeneralLevelEditor
+{
+    public class LevelListScrollerController : MonoBehaviour, IEnhancedScrollerDelegate
+    {
+        private List<LevelData> _data;
+        public EnhancedScroller myScroller;
+        public LevelCellView levelCellViewPrefab;
+        public float cellHeight= 100f;
+        void Start()
+        {
+            _data = new List<LevelData>();
+            _data.Add(new LevelData() { levelName = "Lion" });
+            _data.Add(new LevelData() { levelName = "Bear" });
+            _data.Add(new LevelData() { levelName = "Eagle" });
+            _data.Add(new LevelData() { levelName = "Dolphin" });
+            _data.Add(new LevelData() { levelName = "Ant" });
+
+            _data.Add(new LevelData() { levelName = "Cat" });
+            _data.Add(new LevelData() { levelName = "Sparrow" });
+            _data.Add(new LevelData() { levelName = "Dog" });
+            _data.Add(new LevelData() { levelName = "Spider" });
+            _data.Add(new LevelData() { levelName = "Elephant" });
+            _data.Add(new LevelData() { levelName = "Falcon" });
+            _data.Add(new LevelData() { levelName = "Mouse" });
+            for (int i = 0; i < 1000; i++)
+            {
+                _data.Add(new LevelData() { levelName = "Mouse" });
+            }
+            myScroller.Delegate = this;
+            myScroller.ReloadData();
+        }
+        public int GetNumberOfCells(EnhancedScroller scroller) 
+        {
+             return _data.Count; 
+        }
+
+        public float GetCellViewSize(EnhancedScroller scroller, int dataIndex) 
+        {
+
+             return cellHeight; 
+        }
+
+        public EnhancedScrollerCellView GetCellView(EnhancedScroller scroller, int dataIndex, int cellIndex)
+        {
+            LevelCellView cellView = scroller.GetCellView(levelCellViewPrefab) as LevelCellView;
+            cellView.name = _data[dataIndex].levelName;
+            cellView.onSelected += CellViewSelected;
+            cellView.SetData(dataIndex, _data[dataIndex]);
+
+            return cellView;
+        }
+
+
+        private void CellViewSelected(EnhancedScrollerCellView cellView)
+        {
+            if (cellView == null)
+            {
+               
+            }
+            else
+            {
+                // get the selected data index of the cell view
+                var selectedDataIndex = (cellView as LevelCellView).DataIndex;
+
+                // loop through each item in the data list and turn
+                // on or off the selection state. This is done so that
+                // any previous selection states are removed and new
+                // ones are added.
+                for (var i = 0; i < _data.Count; i++)
+                {
+                    _data[i].Selected = (selectedDataIndex == i);
+                }
+            }
+        }
+    }
+}
