2c94f35ed4a91caf6f6ef82e13c30486f7dcd6e8
Zhang haotian <zhtroy@sina.com>
2018-04-27 15:23:18 +0800

    add json dlls

--- Assets/LevelEditor/Scripts/LevelData.cs
+++ Assets/LevelEditor/Scripts/LevelData.cs
@@ -1,31 +1,11 @@
-﻿using System;
+using System;
+using Wooga.Foundation.Json;
 
 namespace GeneralLevelEditor
 {
-    public class LevelData 
-    { 
-        #region 
-            
-        #endregion
-
-        public event Action<bool> selectedChanged;
-
-        private bool _selected;
-        public bool Selected
-        {
-            get { return _selected; }
-            set
-            {
-                // if the value has changed
-                if (_selected != value)
-                {
-                    // update the state and call the selection handler if it exists
-                    _selected = value;
-                    if (selectedChanged != null) selectedChanged(_selected);
-                }
-            }
-        }
-
+    //纯关卡数据部分
+    public partial class LevelData 
+    {
         public string levelName;
     }
 }
