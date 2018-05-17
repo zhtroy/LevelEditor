160ff3686032e99c90f24ee5dc63d8efafd22654
Zhang haotian <zhtroy@sina.com>
2018-05-17 18:15:57 +0800

    删除prefab ，解决了play 后场景修改的问题

---
+++ Assets/LevelEditor/Scripts/GUIDTool.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using UnityEditor;
+using UnityEngine;
+
+public class GUIDtool : EditorWindow
+{
+
+    string _guid;
+    [MenuItem("LevelEditor/GetAssetByGUID")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(GUIDtool));
+    }
+
+    private void OnGUI()
+    {
+        EditorGUILayout.BeginVertical();
+        _guid = EditorGUILayout.TextField(_guid);
+
+        bool getbtn = GUILayout.Button("get");
+        if (getbtn)
+        {
+            
+             var a =AssetDatabase.LoadAssetAtPath<UnityEngine.Object>(AssetDatabase.GUIDToAssetPath(_guid));
+            Selection.activeObject = a;
+        }
+
+        EditorGUILayout.EndVertical();
+    }
+}
